d03b38c5eea32fc2646e993621633ea02f5984ad
jortiz <josepho011235@gmail.com>
2017-02-23 10:48:20 -0500

    added basic templating

--- main.rb
+++ main.rb
@@ -1,4 +1,24 @@
 require 'sinatra'
 get '/' do
-    "Just Do It"
+  slim :index
 end
+
+__END__
+
+@@layout
+doctype html
+html
+  head
+    meta charset="utf-8"
+    title Just Do It
+    link rel="stylesheet" media="screen, projection" href="/style.css"
+    /[if lt IE9]
+    script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
+  body
+    h1 Just Do It
+    == yield
+
+@@index
+h2 My Tasks
+ul.tasks
+ li Get Milk
